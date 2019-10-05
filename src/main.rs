b49777d010f42c80f88cf96668788bd9bc1381fd
Griffone <thegriffones@gmail.com>
2019-10-06 01:28:51 +0200

    create a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate winit;
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = winit::EventsLoop::new();
+    let window = winit::Window::new(&events_loop).unwrap();
+
+	window.set_title("GERS");
+
+    events_loop.run_forever(|event| {
+        match event {
+            winit::Event::WindowEvent {
+              event: winit::WindowEvent::CloseRequested,
+              ..
+            } => winit::ControlFlow::Break,
+            _ => winit::ControlFlow::Continue,
+        }
+    });
 }
